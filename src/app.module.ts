4122848d05ce1669ffdb944aed9dcf12eef368ac
Bianca Iusco <bianca.iusco@radically.digital>
2023-09-11 19:20:38 +0100

    get all tasks endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TasksModule } from './tasks/tasks.module';
+import { TasksController } from './tasks/tasks.controller';
 
 @Module({
-  imports: [],
+  imports: [TasksModule],
+  controllers: [TasksController],
 })
-export class AppModule { }
+export class AppModule {}
