2a9f8831d7f8b86b4e787bab79a9b028539675f9
Bianca Iusco <bianca.iusco@radically.digital>
2023-09-20 19:35:28 +0100

    delete and update

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -4,16 +4,20 @@ import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './tasks.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { log } from 'console';
 
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [];
+
   getAllTasks(): Task[] {
     return this.tasks;
   }
+
   getTaskById(id: string): Task {
     return this.tasks.find((task) => task.id === id);
   }
+
   createTask(createTaskDto: CreateTaskDto): Task {
     const { title, description } = createTaskDto;
     const task: Task = {
@@ -25,6 +29,16 @@ export class TasksService {
 
     this.tasks.push(task);
 
+    return task;
+  }
+
+  deleteTask(id: string): void {
+    this.tasks = this.tasks.filter((item) => item.id !== id);
+  }
+
+  updateTaskStatus(id: string, status: TaskStatus): Task {
+    const task =this.getTaskById(id);
+    task.status = status;
     return task;
   }
 }
