982c4647be4e25545faf9fa7a35fb144adee8955
Bianca Iusco <bianca.iusco@radically.digital>
2023-11-06 22:32:59 +0000

    bcrypt and signin feature

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
 /* eslint-disable prettier/prettier */
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersRepository } from './users.repository';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import * as bcrypt from 'bcrypt';
 
 
 @Injectable()
@@ -9,7 +10,17 @@ export class AuthService {
     constructor(
         private usersRepository: UsersRepository,
     ) { }
+
     async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         return this.usersRepository.createUser(authCredentialsDto);
+    }
+
+    async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+        const { username, password } = authCredentialsDto;
+        const user = await this.usersRepository.findOne({ where: { username } });
+
+        if (user && (await bcrypt.compare(password, user.password))) {
+            return 'success'
+        } else { throw new UnauthorizedException('Wrong login credentials') }
     }
 }
